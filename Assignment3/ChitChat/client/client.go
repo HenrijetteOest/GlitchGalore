6f40a6ed6e8697e51253eea3f20a82f164b1e0c6
Laura <llsa@itu.dk>
2025-10-28 20:00:38 +0100

    Feat: System.log
    Co-authored-by: Thyme-Cat <Thyme-Cat@users.noreply.github.com>
    Co-authored-by: Green-Peber <HenrijetteOest@users.noreply.github.com>

--- Assignment3/ChitChat/client/client.go
+++ Assignment3/ChitChat/client/client.go
@@ -32,7 +32,7 @@ type ChitChatter struct {
 	Name string
 }
 
-/* Creates a ChitChat User with random id and name*/
+/* Creates a ChitChat User with random id and name */
 func makeRandomClient() ChitChatter {
 	// generate a random id
 	randomID := int32(rand.Intn(999)+1)
@@ -65,12 +65,11 @@ func syncLamport(serverLamport int32) {
 	localLamport = max(localLamport, serverLamport) + 1
 }
 
-/* JoinChat function call with lamport increments*/
+/* JoinChat function call with lamport increments */
 func localJoinChat(client pb.ChitChatServiceClient, localChitChatter ChitChatter, isActivePointer *bool) grpc.ServerStreamingClient[pb.ChitChatMessage] {
 
 	incrementLamport()	
 	
-	// below fileLog done
 	fileLog.Printf("/ Client %d / Join Chat Request / Lamport %d", localChitChatter.ID, localLamport)
 	
 	stream1, err := client.JoinChat(context.Background(), &pb.User{Id: localChitChatter.ID, Name: localChitChatter.Name, Lamport: localLamport})
@@ -86,14 +85,13 @@ func localLeaveChat(client pb.ChitChatServiceClient, localChitChatter ChitChatte
 
 	incrementLamport()
 
-	// below fileLog done
 	fileLog.Printf("/ Client %d / Leave Chat Request / Lamport %d", localChitChatter.ID, localLamport)
 
 	client.LeaveChat(context.Background(), &pb.User{Id: localChitChatter.ID, Name: localChitChatter.Name, Lamport: localLamport})
 	*isActivePointer = false
 }
 
-// receive proto ChitChatMessages from the stream
+// receive ChitChatMessages from the stream
 // Receives messages as long as the stream is active,
 // which is tracked by the isActivePointer
 func receiveMessages(msgStream grpc.ServerStreamingClient[pb.ChitChatMessage], isActivePointer *bool, localChitChatter ChitChatter) {
@@ -102,30 +100,27 @@ func receiveMessages(msgStream grpc.ServerStreamingClient[pb.ChitChatMessage], i
 		if err == io.EOF {
 			break
 		}
-		//log.Printf("local lamport: %d and lamport from server: %d", localLamport, msg.Lamport) //homemade debug
+	
 		syncLamport(msg.Lamport)
 
-		// fileLog below done
+
 		fileLog.Printf("/ Client %d / Received [Client %d]'s Message from Server / Lamport %d", localChitChatter.ID, msg.User.Id, localLamport)
-		//termLog.Printf("%s at logical time %d: %s", msg.User.Name, msg.Lamport, msg.Message)
 		termLog.Println(msg.Message)
-		//log.Printf("local lamport after sync mechanism: %d", localLamport) //homemade debug
+		
 	}
 }
 
-// Sends a proto ChitChatMessage to the Server
+// Sends a ChitChatMessage to the Server
 // Increments the lamport making the request
 func localSendMessage(client pb.ChitChatServiceClient, localChitChatter ChitChatter, message string) {
 	incrementLamport()
 
-	// fileLog below done
 	fileLog.Printf("/ Client %d / Send Message / Lamport %d", localChitChatter.ID, localLamport)
 
 	client.Publish(context.Background(), &pb.ChitChatMessage{User: &pb.User{Id: localChitChatter.ID, Name: localChitChatter.Name, Lamport: localLamport}, Message: message, Lamport: localLamport})
 }
 
-// Sends a total of three proto ChitChatMessages 
-// Uses the
+// Sends a total of 50 proto ChitChatMessages 
 func SendMessageLoop(client pb.ChitChatServiceClient, localChitChatter ChitChatter) {
 	for i := 0; i < 50; i++ {
 
@@ -136,7 +131,7 @@ func SendMessageLoop(client pb.ChitChatServiceClient, localChitChatter ChitChatt
 			log.Fatalf("Not working in messageLoop")
 		}
 		localSendMessage(client, localChitChatter, message)
-		//time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
+		time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 	}
 }
 
@@ -147,9 +142,9 @@ func main() {
 	}
 	defer file.Close()
 
-	// Create two independent loggers
+	/* Create two independent loggers, one for logging to system.log and one for logging in terminal */
 	fileLog = log.New(file, "",log.Ldate|log.Ltime)
-	termLog = log.New(os.Stdout, "", 0) // plain chat-style output
+	termLog = log.New(os.Stdout, "", 0) 
 
 	/* Lamport Timestamp */
 	localLamport = 0
@@ -166,11 +161,10 @@ func main() {
 	defer conn.Close()
 	client := pb.NewChitChatServiceClient(conn)
 
-	// make a user with random name and random id
+	/* make a user with random name and random id */
 	localChitChatter := makeRandomClient()
 
 	/* join chat method call */
-
 	stream1 := localJoinChat(client, localChitChatter, isActivePointer)
 
 	go receiveMessages(stream1, isActivePointer, localChitChatter)
@@ -181,47 +175,7 @@ func main() {
 
 	localLeaveChat(client, localChitChatter, isActivePointer)
 
-	// Below for loop lets the client live forever!
-	// migth be irrelevant
 	for {
 	}
 
 }
-
-/* FREEZER */
-
-// Below loop is where a client receives a response to a joinChat request
-/*
-	for {
-		msg, err := stream1.Recv()
-
-		if err == io.EOF {
-			break
-			//log.Println("Something went wrong with receiving from stream: ")
-		}
-		log.Println("JoinChat: ", msg.Message)
-	}*/
-
-// hardcoded forloop for testing that a client can actually leave the chat
-
-// older methods we might be able to reuse or use as guides
-//client.SendMessage(context.Background(), &proto.ChitChatMessage{User: &proto.User{Id: 1, Name: "Alice"}, Message: "Hello World!", Lamport: 10})
-//Stream.Send(&proto.ChitChatMessage{User: &proto.User{Id: 1, Name: "Alice"}, Message: "This is a message", Lamport: 10})
-
-/*OLD recieve message loop*/
-
-/*
-
-//hardcoded forloop for testing that a client can actually leave the chat
-//this happens after a client has received 2 broadcasts
-
-for i := 0; i < 2; i++ {
-		msg, err := stream1.Recv()
-
-		if err == io.EOF {
-			break
-		}
-		log.Println(msg.Message)
-
-	}
-*/
--- Assignment3/ChitChat/server/server.go
+++ Assignment3/ChitChat/server/server.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"sync"
 	"os"
+	"sync"
 	"time"
-	
+
 	"google.golang.org/grpc"
 
 	pb "ChitChat/grpc" //pb used to be proto
@@ -16,7 +16,6 @@ import (
 
 var fileLog *log.Logger
 var termLog *log.Logger
-
 
 // The ChitChatServer containing:
 // UnimplementedChitChatServiceServer (which makes go happy),
@@ -29,9 +28,8 @@ type ChitChatServer struct {
 	streamStart chan struct{}
 }
 
-
 // JoinChat receives a proto ChitChatMessage stream and a proto user
-// Adds the received stream with the User ID to the global map of active user streams
+// Adds the received stream with the User ID to the server's map of active user streams
 
 // The join chat method, whereby a client joins the chat
 // The server then saves the client stream in an array of streams
@@ -41,7 +39,7 @@ func (s *ChitChatServer) JoinChat(req *pb.User, stream pb.ChitChatService_JoinCh
 	s.userStreams[req.Id] = stream //map version
 	s.streamStart <- struct{}{}
 
-	//log.Printf("Local Lamport in JoinChat before sync: %d and message lamport: %d", s.lamport, req.Lamport)
+	
 	if s.lamport < req.Lamport {
 		s.lamport = req.Lamport + 1
 	} else {
@@ -59,7 +57,6 @@ func (s *ChitChatServer) JoinChat(req *pb.User, stream pb.ChitChatService_JoinCh
 	select {} // homemade from chat
 }
 
-// 
 func (s *ChitChatServer) LeaveChat(ctx context.Context, req *pb.User) (*pb.Empty, error) {
 	//log.Printf("Local Lamport in LeaveChat before sync: %d and message lamport: %d", s.lamport, req.Lamport)
 	if s.lamport < req.Lamport {
@@ -78,17 +75,15 @@ func (s *ChitChatServer) LeaveChat(ctx context.Context, req *pb.User) (*pb.Empty
 	return &pb.Empty{}, nil
 }
 
-
 func (s *ChitChatServer) Publish(ctx context.Context, req *pb.ChitChatMessage) (*pb.Empty, error) {
-	//log.Printf("Local Lamport in Publish before sync: %d and message lamport: %d", s.lamport, req.User.Lamport)
-	//sync mechanism works
+	
 	if s.lamport < req.User.Lamport {
 		s.lamport = req.User.Lamport + 1
 	} else {
 		s.lamport = s.lamport + 1
 	}
 	fileLog.Printf("/ Server / Receive message from user %d / Lamport %d", req.User.Id, s.lamport)
-	//log.Printf("Lamport in Publish after sync: %d", s.lamport)
+	
 	msg := fmt.Sprintf("%s at logical time %d: %s", req.User.Name, s.lamport, req.GetMessage())
 	s.Broadcast(req.GetUser(), msg)
 	return &pb.Empty{}, nil
@@ -98,7 +93,6 @@ func (s *ChitChatServer) Publish(ctx context.Context, req *pb.ChitChatMessage) (
 // Broadcasts a message to all clients in the list of user streams
 // The userStreams array is updated in JoinChat to contain multiple streams
 func (s *ChitChatServer) Broadcast(req *pb.User, msg string) error {
-	//fmt.Println("Number of clients with 'open' streams: ", len(s.userStreams)) // homemade debugging
 
 	waitGroup := sync.WaitGroup{}
 	done := make(chan int32)
@@ -114,6 +108,10 @@ func (s *ChitChatServer) Broadcast(req *pb.User, msg string) error {
 			defer waitGroup.Done()
 			err := s.userStreams[clientId].Send(broadcastMsg)
 			if err != nil {
+				if errors.Is(err, io.EOF) {
+					s.userStreams
+					return nil
+				}
 				log.Printf("error trying to send to client: %d (stream closed) %v", clientId, err)
 			}
 			fileLog.Printf("/ Server / Broadcast message to user %d from user %d / Lamport %d", clientId, req.Id, s.lamport)
@@ -128,8 +126,7 @@ func (s *ChitChatServer) Broadcast(req *pb.User, msg string) error {
 	return nil
 }
 
-
-// Calls for starting the server and create (?) the log file
+// Calls for starting the server and instantiating logger objects
 func main() {
 
 	file, e := os.OpenFile("system.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -139,28 +136,28 @@ func main() {
 	defer file.Close()
 
 	// Create two independent loggers
-	fileLog = log.New(file, "",log.Ldate|log.Ltime)
+	fileLog = log.New(file, "", log.Ldate|log.Ltime)
 	termLog = log.New(os.Stdout, "", 0) // plain chat-style output
 
 	server := &ChitChatServer{lamport: 0, userStreams: make(map[int32]pb.ChitChatService_JoinChatServer), streamStart: make(chan struct{})}
 	server.start_server()
 
 	//fileLog.Printf("/ Server / before time sleep / %d", server.lamport)
-	time.Sleep(45 * time.Second)
+	//time.Sleep(45 * time.Second)
 	//server.shutdown_server()
 
 	/*
-	//for shutting down the server after some time 
-	//maybe this function should be renamed to start_and_shutdown_server
-	time.Sleep(45 * time.Second)
-	fileLog.Printf("/ Server / Shutdown / %d", server.lamport)
-	termLog.Println("Server shutting down on port 5050...")
-	//server.Shutdown()
-	//Stop()
+		//for shutting down the server after some time
+		//maybe this function should be renamed to start_and_shutdown_server
+		time.Sleep(45 * time.Second)
+		fileLog.Printf("/ Server / Shutdown / %d", server.lamport)
+		termLog.Println("Server shutting down on port 5050...")
+		//server.Shutdown()
+		//Stop()
 	*/
 }
 
-// Method for starting the server 
+// Method for starting the server
 func (s *ChitChatServer) start_server() {
 
 	listener, err := net.Listen("tcp", ":5050")
@@ -173,25 +170,24 @@ func (s *ChitChatServer) start_server() {
 
 	fileLog.Printf("/ Server / Startup / %d", s.lamport)
 	termLog.Println("Server running on port 5050...")
-	
-	
+
+	// Below method for gracefully stopping the server comes from https://github.com/grpc/grpc-go/blob/master/examples/features/gracefulstop/server/main.go
 	go func() {
 		<-s.streamStart
-		time.Sleep(1 * time.Second)
-		fileLog.Println("/ Server / initiating graceful shutdown / %d", s.lamport)
-		termLog.Println("initiating graceful shutdown...")
-		timer := time.AfterFunc(10 * time.Second, func() {
+		time.Sleep(500 * time.Second)
+		fileLog.Printf("/ Server / initiating graceful shutdown / %d", s.lamport)
+		termLog.Println("Server initiating graceful shutdown...")
+		timer := time.AfterFunc(15*time.Second, func() {
 			fileLog.Printf("/ Server / force stop / %d ", s.lamport)
 			termLog.Println("Server couldn't stop gracefully in time. Doing a force stop")
 			grpcServer.Stop()
 		})
 		defer timer.Stop()
-		grpcServer.GracefulStop();
-		fileLog.Println("/ Server / Stopped gracefully / %d", s.lamport)
+		grpcServer.GracefulStop()
+		fileLog.Printf("/ Server / Stopped gracefully / %d", s.lamport)
 		//termLog.Println("Server stopped gracefully")
 	}()
-	
-	
+
 	err = grpcServer.Serve(listener) //activates the server
 	if err != nil {
 		log.Fatalf("Did not work in server")
@@ -199,37 +195,11 @@ func (s *ChitChatServer) start_server() {
 }
 
 func (s *ChitChatServer) shutdown_server() {
-    if s != nil {
-        fileLog.Printf("/ Server / Shutdown / %d", s.lamport)
-        termLog.Println("Server shutting down on port 5050...")
-        //s.grpcServer.GracefulStop()
-    } else {
-        termLog.Println("No server instance to shut down.")
-    }
-}
-
-
-
-
-/* FREEZER */
-/*
-// Broadcasts a message to all clients in the list of user streams
-// The userStreams array is updated in JoinChat to contain multiple streams
-func (s *ChitChatServer) Broadcast(req *pb.UserLamport, msg string) error {
-	fmt.Println("Number of clients with 'open' streams: ", len(s.userStreams)) // homemade debugging
-
-	// Go through all streams in the userStream list and send them the same message
-
-	broadcastMsg := &pb.ChitChatMessage{Message: msg}
-
-	for key, _ := range s.userStreams {
-		err := s.userStreams[key].Send(broadcastMsg)
-		if err != nil {
-			log.Printf("error trying to send to clien: %d (stream closed) %v", key, err)
-			continue
-		}
+	if s != nil {
+		fileLog.Printf("/ Server / Shutdown / %d", s.lamport)
+		termLog.Println("Server shutting down on port 5050...")
+		//s.grpcServer.GracefulStop()
+	} else {
+		termLog.Println("No server instance to shut down.")
 	}
-
-	return nil
-}*/
-
+}
