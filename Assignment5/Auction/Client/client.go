2e9f9bc028726972abe66d07f5be0def0b90e7f2
Laura <llsa@itu.dk>
2025-11-22 11:39:38 +0100

    commit: switch liveshare host
    Co-authored-by: Thyme-Cat <Thyme-Cat@users.noreply.github.com>
    Co-authored-by: Green-Peber <HenrijetteOest@users.noreply.github.com>

--- Assignment5/Auction/Client/client.go
+++ Assignment5/Auction/Client/client.go
@@ -1,70 +1,67 @@
 package main
 
-
-import(
+import (
 	"context"
 	"fmt"
 	"log"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"math/rand"
 	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	pb "Auction/grpc"
 )
 
-
-
 type AuctionClient struct {
 	// client pb.AuctionServiceClient
-	ID		int32
-	My_Bid 	int32
+	ID     int32
+	My_Bid int32
 }
 
 func main() {
-	fmt.Println("Starting Auction Client...")	
+	fmt.Println("Starting Auction Client...")
 	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Client could not connect: %v", err)
 	}
-	
+
 	defer conn.Close()
 	client := pb.NewAuctionServiceClient(conn)
 
-	LocalClient := &AuctionClient{
-		ID: int32(1), 		// TODO: In a real scenario, this would be unique for each client
-		My_Bid: int32(rand.Intn(99)+1), // Random start bid, between 1 and 100
+	LocalBidder := &AuctionClient{
+		ID:     int32(1),                 // TODO: In a real scenario, this would be unique for each client
+		My_Bid: int32(rand.Intn(99) + 1), // Random start bid, between 1 and 100
 	}
 
-	go PlaceBid(client, LocalClient)
+	go PlaceBid(client, LocalBidder)
 
 	// Keep the client running
 	select {}
 }
 
-
 // bidding logic
 /* Only resets it's bidding price if it asks and the bidding is over  */
 func PlaceBid(client pb.AuctionServiceClient, LocalClient *AuctionClient) {
-	for i := 0; i < 30; i++{
+	for i := 0; i < 30; i++ {
 		// Get current status of the action
 		res, err := client.Result(context.Background(), &pb.Empty{})
 		if err != nil {
 			fmt.Printf("Failed to get result from grpc call: %v", err)
 		}
-		
-		if res.ItemSold == true { 	// If the auction is over, then reset price
-			LocalClient.My_Bid = int32(rand.Intn(99)+1) // Random start bid, between 1 and 100
 
-		} else {  				// Auction round is still ongoing!
-			if res.HighestBid == 0 {
-				LocalClient.My_Bid = int32(rand.Intn(99)+1) 
-			}
+		if res.ItemSold == true { // If the auction is over, then reset price
+			LocalClient.My_Bid = int32(rand.Intn(99) + 1) // Resest bid field with a new random start bid, between 1 and 100
+
+		} else { // Auction round is still ongoing!
+			/*if res.HighestBid == 0 {
+				LocalClient.My_Bid = int32(rand.Intn(99) + 1)
+			}*/
 			if res.HighestBid >= LocalClient.My_Bid {
 				// Increase our bid price and then bid
-				LocalClient.My_Bid = res.HighestBid + int32(rand.Intn(59)+5) // 1  
+				LocalClient.My_Bid = res.HighestBid + int32(rand.Intn(49)+1) // increment bid by 1-50
 				fmt.Printf("	Client %d is increasing bid to %d\n", LocalClient.ID, LocalClient.My_Bid)
-			} 
+			}
 			BidCall(client, LocalClient) // Do the grpc call
 		}
 		time.Sleep(time.Duration(int32(rand.Intn(5))) * time.Second)
@@ -72,34 +69,33 @@ func PlaceBid(client pb.AuctionServiceClient, LocalClient *AuctionClient) {
 }
 
 func BidCall(client pb.AuctionServiceClient, LocalClient *AuctionClient) {
-	// res, err := 
+	// res, err :=
 	// Bid() should return some things, but that doesn't work at the moment
 	client.Bid(context.Background(), &pb.Bidder{
 		Bid: LocalClient.My_Bid,
-		Id: LocalClient.ID,
+		Id:  LocalClient.ID,
 	})
 
 	fmt.Printf("Client %d placed a bid of %d\n", LocalClient.ID, LocalClient.My_Bid)
 
 	/*
-	if err == nil {
-		fmt.Println("something went wrong in BidCall")
-	}
+		if err == nil {
+			fmt.Println("something went wrong in BidCall")
+		}
 
-	if res.Status == "SUCCESS" {
-		fmt.Printf("SUCCESS: Client %d placed a bid of %d\n", LocalClient.ID, LocalClient.My_Bid)
-	} else if res.Status == "FAIL" {
-		fmt.Printf("FAIL: Client %d failed to place a bid \n", LocalClient.ID)
-	}*/
+		if res.Status == "SUCCESS" {
+			fmt.Printf("SUCCESS: Client %d placed a bid of %d\n", LocalClient.ID, LocalClient.My_Bid)
+		} else if res.Status == "FAIL" {
+			fmt.Printf("FAIL: Client %d failed to place a bid \n", LocalClient.ID)
+		}*/
 }
 
-
 /* FREEZER */
-	// Place a bid
-	/*
+// Place a bid
+/*
 	Case 1: 	current bid < our_bid  &&   item NOT sold	-->  just bid
 	Case 2: 	current bid > our_bid  && 	item NOT sold	-->  increase our price and then bid
 	Case 3:		current bid < our_bid  && 	item IS sold	-->  lower our start price
 	Case 4:  	current bid > our_bid  && 	item IS sold	-->  lower our start price
-	
-	*/
+
+*/
--- Assignment5/Auction/Server/server.go
+++ Assignment5/Auction/Server/server.go
@@ -2,44 +2,41 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"time"
-	"math/rand"
 	"errors"
-	"net"
+	"fmt"
 	"log"
+	"net"
+	"time"
 
 	"google.golang.org/grpc"
 
 	pb "Auction/grpc"
-
 )
-
 
 type AuctionServer struct {
 	pb.UnimplementedAuctionServiceServer
-	ID 					int32
-	LeaderID 			int32
+	ID       int32
+	LeaderID int32
 	//SynchronousID 	int32 // For Server backups
 	//OtherServers 		map[int32]	//key: id //value: status i bidder?	// keep track of other servers //for server backup
-	BestBid 			HighestBidder  // The best bid so far
-	RegisteredClients   map[int32]bool // Keep track of registered Clients
-	AuctionOngoing		bool
+	BestBid           HighestBidder  // The best bid so far
+	RegisteredClients map[int32]bool // Keep track of registered Clients
+	AuctionOngoing    bool
 }
 
 type HighestBidder struct {
-	BidderID	int32
-	BidAmount	int32
+	BidderID  int32
+	BidAmount int32
 }
 
 func main() {
 	fmt.Println("Starting Auction Server...")
 	server := &AuctionServer{
-		ID:       1,
-		LeaderID: 1,
-		BestBid:  HighestBidder{BidderID: -1, BidAmount: 0},
+		ID:                1,
+		LeaderID:          1,
+		BestBid:           HighestBidder{BidderID: -1, BidAmount: 0},
 		RegisteredClients: make(map[int32]bool),
-		AuctionOngoing: false,
+		AuctionOngoing:    false,
 	}
 	server.start_server()
 
@@ -58,7 +55,7 @@ func (s *AuctionServer) start_server() {
 	pb.RegisterAuctionServiceServer(grpcServer, s)
 	fmt.Printf("Auction Server %d listening on port 5050\n", s.ID)
 
-	go func(){
+	go func() {
 		if err := grpcServer.Serve(lis); err != nil {
 			log.Fatalf("Failed to serve: %v", err)
 		}
@@ -67,46 +64,45 @@ func (s *AuctionServer) start_server() {
 
 /* Starts each bidding round by changing Auction Ongoing to true or false and updating the best bid thereafter  */
 func (s *AuctionServer) StartAndEndBiddingRound() {
-	for i := 1; i < 6; i++ { 	
-			// Total items to be bid on before the auction ends
+	for i := 1; i < 6; i++ {
+		// Total items to be bid on before the auction ends
 		s.BestBid.BidderID = -1
-		s.BestBid.BidAmount = 0		// reset the highest bidder
-		
-		s.AuctionOngoing = true			// Auction round begins
+		s.BestBid.BidAmount = 0 // reset the highest bidder
+
+		s.AuctionOngoing = true // Auction round begins
 		fmt.Printf("	Round %d of auction has begun \n", i)
-		time.Sleep(10 * time.Second)	// Auction round duration
-		
-		s.AuctionOngoing = false		// Auction round ends
+		time.Sleep(10 * time.Second) // Auction round duration
+
+		s.AuctionOngoing = false // Auction round ends
 		fmt.Printf("	Round %d of auction is over, winning bid: %d by client: %d \n", i, s.BestBid.BidAmount, s.BestBid.BidderID)
-		time.Sleep(time.Duration(int32(rand.Intn(5))) * time.Second)	// Next item to be sold is prepared
+		time.Sleep(6 * time.Second) // Next item to be sold is prepared	// time.Duration(int32(rand.Intn(5)))
 	}
 	fmt.Println("The auction is now over!")
 }
 
 /* Does not properly return the pb.BidResponse to Client (don't know why yet) */
 func (s *AuctionServer) Bid(ctx context.Context, bidder *pb.Bidder) (*pb.BidResponse, error) {
-	err := errors.New("EXCEPTION")	// makes the exception message
-	
+	err := errors.New("EXCEPTION") // makes the exception message
+
 	if s.RegisteredClients[bidder.Id] != true {
 		s.RegisteredClients[bidder.Id] = true
 		// add grpc call to another server to update the database
 	}
-	
-	if bidder.Bid > s.BestBid.BidAmount && s.AuctionOngoing == true{
+
+	if bidder.Bid > s.BestBid.BidAmount && s.AuctionOngoing == true {
 		s.BestBid.BidAmount = bidder.Bid
 		s.BestBid.BidderID = bidder.Id
 		// return success or err (exception)
-		return &pb.BidResponse{Status: "SUCCESS"}, err 
-	} 
-	 
+		return &pb.BidResponse{Status: "SUCCESS"}, err
+	}
+
 	return &pb.BidResponse{Status: "FAIL"}, err //used to be nil
 }
 
 /* Returns the highest bid and whether the item has been sold yet or not   */
 func (s *AuctionServer) Result(ctx context.Context, empty *pb.Empty) (*pb.ResultResponse, error) {
 	return &pb.ResultResponse{
-		HighestBid: s.BestBid.BidAmount,	
-		ItemSold: !s.AuctionOngoing,		// if auction is ongoing then the Item hasn't been sold
-	}, nil	
+		HighestBid: s.BestBid.BidAmount,
+		ItemSold:   !s.AuctionOngoing, // if auction is ongoing then the Item hasn't been sold
+	}, nil
 }
-
